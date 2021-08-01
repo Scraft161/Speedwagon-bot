fda8d6e581620bdf8e2e65ad8d8a921f433e59e4
Scraft161 <scraft161@gmail.com>
2021-08-01 22:49:32 +0200

    Clean up app.js and add more comments. finally fix quote system.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Discord libraries
+// Discord integration libraries
 const Discord = require('discord.js');
 const client  = new Discord.Client();
 // Bot specific libraries
@@ -6,14 +6,18 @@ const parser   = require('./parser.js');		// Parser
 const commands = require('./commands.js');	// Commands list and code
 const conf     = require('./config.json');	// Config
 
+// Show a message when the bot is ready
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
 });
 
+// message handler
 client.on('message', msg => {
 	if (msg.content.startsWith(conf.prefix)) {
 		let newMsg = commands.run(parser.process(msg.content));
 
+		// In order to prevent some crashes we filter out messages that aren't of type string.
+		// If your module is giving the message below try casting to string before returning.
 		if (typeof(newMsg) == "string") {
 			msg.channel.send(newMsg);
 		} else {
@@ -25,31 +29,3 @@ client.on('message', msg => {
 // Login with the token provided in the config
 console.log("connecting to Discord ...");
 client.login(conf.token);
-
-/*
- * external function issue workaround
-*/
-
-// commands
-/*
-const quote = () => {
-	const quotes = {
-		"people": {
-			"Scraft161": [
-				"I hate my life.",
-				"When all else fails, degeneracy is the only option left.",
-				"Don't quote me on that.",
-				"I told you not to quote me on that."
-			],
-			"Giorno Giovanna": [
-				"And that’s why I turned one of your pistols into a banana. It’s your last meal. Take your time… enjoy it."
-			],
-			"Jotaro Kujo": [
-				"You truly are the lowest scum in history. You can’t pay back what you owe with money."
-			]
-		}
-	};
-
-	return Math.round(Math.random() * (Object.keys(quotes.people).length - 1));
-};
-*/
--- commands.js
+++ commands.js
@@ -5,7 +5,7 @@ const run = (commandObj) => {
 
 	switch (commandObj.tokens[0]) {
 		case "quote":
-			return /*quote(tokens[1])*/ "Why isn't my code working? (this includes the quote system)\n- Scraft161";
+			return quote();
 		case "rate":
 			return rate(commandObj);
 		case "source":
@@ -15,9 +15,29 @@ const run = (commandObj) => {
 	}
 };
 
-/*const quote = (commandObj) => {
+const quote = () => {
 	// TODO: quote system
-}*/
+	const quotes = [
+		"I hate my life.\n- Scraft161",
+		"Why isn't my code working? (this includes the quote system)\n- Scraft161",
+		"When all else fails, degeneracy is the only option left.\n- Scraft161",
+		"I really should stop quoting myself.\n- Scraft161",
+		"Don't quote me on that.\n- Scraft161",
+		"I told you not to quote me on that.\n- Scraft161",
+		"And that’s why I turned one of your pistols into a banana. It’s your last meal. Take your time… enjoy it.\n- Giorno Giovanna",
+		"You truly are the lowest scum in history. You can’t pay back what you owe with money.\n- Jotaro Kujo",
+		"Just shut up already. I have nothing more to say to you. You’re way too pathetic… I’m done wasting my breath.\n- Jotaro Kujo",
+		"I covered you with gasoline so I can track your smell. I feel bad, kicking a guy when he’s down, but… Nah, I don´t feel bad about this at all. My friends and a lot of bystanders are dead because of you.\n- Jotaro Kujo",
+		"This ends here! You will not leave this place alive!\n- Jonathan Joestar",
+		"When I was a kid, I saw Mona Lisa from my grammar school art book….The fist time I saw her, with her hands on her knee…how do I say this…I had a boner.\n- Yoshikage Kira",
+		"Go out there and take this useless world for all you can get.\n Dio Brando",
+		"Right about now, you’re probably wondering who I am. Allow me to elucidate ya! The name is Robert E. O. Speedwagon! I had a hunch Jonathan’s run of trouble might not be done, so I followed him back home. I like you, Jonathan, so I’m gonna offer up this little tidbit pro bono: living in the gutter, you learn to spot lowlifes pretty quick if you want to see tomorrow. I’ve trained my nose to sort out the bad ones from the good just from a sniff. This fellow reeks of brimstone and blood worse than anyone I’ve ever laid eyes on!\n- Robert E.O. Speedwagon",
+		"Shoot me if you want to! But you better be prepared to face the consequences. The moment you try to pull that trigger, I’ll snap your fingers in half like matchsticks!\n- Joseph Joestar"
+	];
+	const quoteLen = quotes.length - 1;
+
+	return quotes[Math.round(Math.random() * quoteLen)];
+};
 
 const rate = (commandObj) => {
 	if (commandObj.argStr.includes("Jonathan")) {
